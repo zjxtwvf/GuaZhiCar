a95f308677faf38c2778f085d2c6e9a2e5de254c
zjxtwvf <876424571@qq.com>
2018-04-11 22:23:20 +0800

    recycleview 大图显示功能

--- app/src/main/java/com/terminator/zjxtwvf/guazi/view/adapter/HomeAdapter.java
+++ app/src/main/java/com/terminator/zjxtwvf/guazi/view/adapter/HomeAdapter.java
@@ -10,6 +10,7 @@ import android.widget.TextView;
 
 import com.terminator.zjxtwvf.guazi.R;
 import com.terminator.zjxtwvf.guazi.model.entity.BannerAdsEntity;
+import com.terminator.zjxtwvf.guazi.model.entity.BannerImageUrlEntity;
 import com.terminator.zjxtwvf.guazi.model.entity.CarListEntity;
 import com.terminator.zjxtwvf.guazi.model.entity.RecyclerViewEvent;
 import com.terminator.zjxtwvf.guazi.util.BitmapCacheUtils;
@@ -19,7 +20,10 @@ import com.terminator.zjxtwvf.guazi.view.widget.ImageViewMatchWidth;
 
 import org.greenrobot.eventbus.EventBus;
 
+import java.util.Arrays;
 import java.util.List;
+
+import cn.bingoogolapple.bgabanner.BGABanner;
 
 
 /**
@@ -31,8 +35,9 @@ public class HomeAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
     public static final int DISPALY_BIG = 0 ;
     public static final int DISPALY_SMALL = 1 ;
 
-    public static final int LIST_NORMAL = 0;
-    public static final int LIST_ADS = 1;
+    public static final int LIST_ADS = 0;
+    public static final int LIST_NORMAL = 1;
+    public static final int LIST_NORMAL_BIG = 2;
 
     int mDisMode = DISPALY_SMALL;
     BannerAdsEntity mAdsData;
@@ -57,7 +62,11 @@ public class HomeAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
         if(0 == position || 6 == position){
             return LIST_ADS;
         }else{
-            return LIST_NORMAL;
+            if(DISPALY_SMALL == mDisMode){
+                return LIST_NORMAL;
+            }else{
+                return LIST_NORMAL_BIG;
+            }
         }
     }
 
@@ -66,14 +75,8 @@ public class HomeAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
         View view;
         RecyclerView.ViewHolder myViewHolder;
         if(viewType == LIST_NORMAL){
-            if(DISPALY_SMALL == mDisMode){
-                view = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_item_post_list,parent,false);
-                myViewHolder = new HomeAdapter.MyViewHolder(view);
-            }else{
-                view = LayoutInflater.from(parent.getContext()).inflate(R.layout.buy_car_list_item_big_mode,parent,false);
-                myViewHolder = new HomeAdapter.MyViewBigModeHolder(view);
-            }
-
+            view = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_item_post_list,parent,false);
+            myViewHolder = new HomeAdapter.MyViewHolder(view);
             view.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
@@ -82,7 +85,18 @@ public class HomeAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
                 }
             });
             return myViewHolder;
-        }else{
+        }else if(viewType == LIST_NORMAL_BIG){
+            view = LayoutInflater.from(parent.getContext()).inflate(R.layout.buy_car_list_item_big_mode,parent,false);
+            myViewHolder = new HomeAdapter.MyViewBigModeHolder(view);
+            view.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(UIUtils.getContext(),SourceDetailActivity.class);
+                    UIUtils.getContext().startActivity(intent);
+                }
+            });
+            return myViewHolder;
+        } else{
             ImageViewMatchWidth imageViewMatchWidth = new ImageViewMatchWidth(UIUtils.getContext());
             MyViewAdHolder adHolder = new MyViewAdHolder(imageViewMatchWidth);
             return  adHolder;
@@ -92,15 +106,15 @@ public class HomeAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
         int index;
+        if(position >= 1 && position < 7){
+            index = position - 1;
+        }else{
+            index = position - 2;
+        }
         if(position == data.size() -1){
             EventBus.getDefault().post(new RecyclerViewEvent());
         }
         if(holder instanceof  HomeAdapter.MyViewHolder){
-            if(position >= 1 && position < 7){
-                index = position - 1;
-            }else{
-                index = position - 2;
-            }
             ((HomeAdapter.MyViewHolder)holder).tvTtile.setText(data.get(index).getTitle());
             ((HomeAdapter.MyViewHolder)holder).tvDateHual.setText(data.get(index).getLicense_date());
             ((HomeAdapter.MyViewHolder)holder).tvPrice.setText(data.get(index).getPrice());
@@ -117,6 +131,20 @@ public class HomeAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
             ViewGroup.LayoutParams params = ((MyViewBigModeHolder)holder).itemView.getLayoutParams();
             params.height = ViewGroup.LayoutParams.WRAP_CONTENT;
             ((MyViewBigModeHolder)holder).itemView.setLayoutParams(params);
+            ((MyViewBigModeHolder)holder).tv_car_title.setText(data.get(index).getTitle());
+            ((MyViewBigModeHolder)holder).tv_dis_desc.setText(data.get(index).getLicense_date());
+            ((MyViewBigModeHolder)holder).tv_price.setText(data.get(index).getPrice());
+            ((MyViewBigModeHolder)holder).tv_price_first.setText("首付"+data.get(index).getFirst_pay());
+            ((MyViewBigModeHolder)holder).tv_position.setText(data.get(index).getCity_name());
+            BGABanner mBanner = ((MyViewBigModeHolder)holder).banner_content;
+            mBanner.setAdapter(new BGABanner.Adapter<ImageView,Object>() {
+                @Override
+                public void fillBannerItem(BGABanner banner, ImageView itemView, Object model, int position) {
+                    BitmapCacheUtils.getInstance().display((ImageView) itemView,
+                            (String)model);
+                }
+            });
+            mBanner.setData(data.get(index).getImage_list(), Arrays.asList("","","","",""));
         }
     }
 
@@ -144,17 +172,25 @@ public class HomeAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
     }
 
     public class MyViewBigModeHolder extends RecyclerView.ViewHolder{
-        ImageView iv;
-        TextView tvTtile;
-        TextView tvDateHual;
-        TextView tvPrice;
+        BGABanner banner_content;
+        TextView  tv_car_title;
+        TextView  tv_dis_desc;
+        TextView  tv_big_mode_tag0;
+        TextView  tv_big_mode_tag1;
+        TextView  tv_price;
+        TextView  tv_price_first;
+        TextView  tv_position;
 
         public MyViewBigModeHolder(View itemView) {
             super(itemView);
-            iv = (ImageView)itemView.findViewById(R.id.iv_item_post_list);
-            tvTtile = (TextView)itemView.findViewById(R.id.tv_item_post_title);
-            tvDateHual = (TextView)itemView.findViewById(R.id.tv_item_post_date_hual);
-            tvPrice = (TextView)itemView.findViewById(R.id.tv_item_post_price);
+            banner_content = (BGABanner)itemView.findViewById(R.id.banner_content);
+            tv_car_title = (TextView)itemView.findViewById(R.id.tv_car_title);
+            tv_dis_desc = (TextView)itemView.findViewById(R.id.tv_dis_desc);
+            tv_big_mode_tag0 = (TextView)itemView.findViewById(R.id.tv_big_mode_tag0);
+            tv_big_mode_tag1 = (TextView)itemView.findViewById(R.id.tv_big_mode_tag1);
+            tv_price = (TextView)itemView.findViewById(R.id.tv_price);
+            tv_price_first = (TextView)itemView.findViewById(R.id.tv_price_first);
+            tv_position = (TextView)itemView.findViewById(R.id.tv_position);
         }
     }
 
